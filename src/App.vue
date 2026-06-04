146f6de7f52c088e030abac5694cbddc68fd24d3
LEV <levvanusin@gmail.com>
2026-06-04 14:03:24 +0300

    dfsd

--- src/App.vue
+++ src/App.vue
@@ -40,7 +40,7 @@
       <!-- Результаты после прохождения -->
       <div v-else-if="quizFinished" class="results">
         <h2>Викторина завершена!</h2>
-        <p>Правильных ответов: {{ score }} из {{ data.length }}</p>
+        <p>Правильных ответов: {{ score * 5 }} из {{ data.length * 5 }}</p>
       </div>
       
       <div v-else-if="errorMessage" class="error">{{ errorMessage }}</div>
