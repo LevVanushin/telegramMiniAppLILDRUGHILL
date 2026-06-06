feddeea6125c387fb5bee070e9690fdf5e0143ed
LEV <levvanusin@gmail.com>
2026-06-06 14:41:30 +0300

    fdfd

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // Данные из настроек проекта Supabase (Project Settings → API)
 const supabaseUrl = "https://hgswdnzhjsejqonxckgt.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhnc3dkbnpoanNlanFvbnhja2d0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MDIxNjA3MiwiZXhwIjoyMDk1NzkyMDcyfQ.DJkaC4rKJT1KttHifSfmH20frimEGqAZFpm1wEdVb1k";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhnc3dkbnpoanNlanFvbnhja2d0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyMTYwNzIsImV4cCI6MjA5NTc5MjA3Mn0.8m-MyDmXVuDcaaCaluQUCvxtGzc0IOb_5pZmwrR1OnY";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
