326ad28bf95a21895ee78071f792633c7bfb8d2d
LEV <levvanusin@gmail.com>
2026-06-06 13:04:53 +0300

    fix last v1.2

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
         <h2>Викторина уже пройдена!</h2>
         <p class="score">Баллы: {{ completedScore * 5 }} из {{ data.length * 5 }}</p>
         <p class="thankyou-message">Благодарим за прохождение. Желаем удачи всем на реальных экзаменах!</p>
-        <p class="armyCaption">lildrughillarmy</p>
+        <p class="armyCaption">lildrughillarmy - off fun pag</p>
       </div>
       
       <quizCard 
