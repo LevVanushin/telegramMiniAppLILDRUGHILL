0dccfd196580c88665aac979f025c5926099d309
LEV <levvanusin@gmail.com>
2026-06-06 13:14:30 +0300

    last v2.2

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
         <h2>Викторина уже пройдена!</h2>
         <p class="score">Баллы: {{ completedScore * 5 }} из {{ data.length * 5 }}</p>
         <p class="thankyou-message">Благодарим за прохождение. Желаем удачи всем на реальных экзаменах!</p>
-        <p class="armyCaption">lildrughillarmy - off fun page</p>
+        <p class="armyCaption">lildrughill army - off fan page</p>
       </div>
       
       <quizCard 
@@ -32,7 +32,7 @@
         <h2>Викторина завершена!</h2>
         <p class="score">Баллы: {{ score * 5 }} из {{ data.length * 5 }}</p>
         <p class="thankyou-message">Благодарим за прохождение. Желаем удачи всем на реальных экзаменах!</p>
-        <p class="armyCaption">lildrughillarmy - off fun page</p>
+        <p class="armyCaption">lildrughill army - off fan page</p>
       </div>
       
       <div v-else-if="errorMessage" class="error">{{ errorMessage }}</div>
