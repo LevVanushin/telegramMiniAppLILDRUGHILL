d1f3e62ffe4ddbed58794d26161a6a3013390f50
Lev <162362951+LevVanushin@users.noreply.github.com>
2026-06-10 22:34:44 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -427,7 +427,7 @@ async function submitDiploma() {
     <div style="position: absolute; top: 320px; left: 0; right: 0;">
       <h2 style="font-size: 30px; font-weight: 600; font-family: Georgia, 'Times New Roman', Times, serif; margin: 0; color: white;">${userName}</h2>
     </div>
-    <div style="position: absolute; bottom: 85px; left: 133px;">
+    <div style="position: absolute; bottom: 90px; left: 133px;">
       <p style="font-size: 20px; text-align:left; font-family: Georgia, 'Times New Roman', Times, serif; font-weight: 500">${new Date().toLocaleDateString()}</p>
     </div>
   `;
