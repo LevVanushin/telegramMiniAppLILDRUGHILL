66c3157bc4793e096261b1a17b71ce9eecb96d8c
LEV <levvanusin@gmail.com>
2026-06-10 23:34:29 +0300

    fdfdf

--- src/App.vue
+++ src/App.vue
@@ -414,8 +414,8 @@ async function submitDiploma() {
   diplomaDiv.style.position = 'absolute';
   diplomaDiv.style.left = '-9999px';
   diplomaDiv.style.top = '0';
-  diplomaDiv.style.width = '1500px';
-  diplomaDiv.style.height = '1200px';
+  diplomaDiv.style.width = '1300px';
+  diplomaDiv.style.height = '1000px';
   diplomaDiv.style.backgroundImage = 'url(/diplom.jpg)';
   diplomaDiv.style.backgroundSize = 'cover';
   diplomaDiv.style.backgroundPosition = 'center';
