f8879a16c47eb4ecc3600d3b6b3df0b2ad5d9c98
Lev <162362951+LevVanushin@users.noreply.github.com>
2026-06-10 22:23:19 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -424,10 +424,10 @@ async function submitDiploma() {
   diplomaDiv.style.textAlign = 'center';
 
   diplomaDiv.innerHTML = `
-    <div style="position: absolute; top: 303px; left: 0; right: 0;">
+    <div style="position: absolute; top: 320px; left: 0; right: 0;">
       <h2 style="font-size: 30px; font-weight: 600; font-family: Georgia, 'Times New Roman', Times, serif; margin: 0; color: white;">${userName}</h2>
     </div>
-    <div style="position: absolute; bottom: 85px; left: 0;">
+    <div style="position: absolute; bottom: 85px; left: 100px;">
       <p style="font-size: 20px; text-align:left; font-weight: 400">${new Date().toLocaleDateString()}</p>
     </div>
   `;
