300523c23fbdf899e455a373c00dc50b99c94020
Lev <162362951+LevVanushin@users.noreply.github.com>
2026-06-10 22:08:39 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -414,7 +414,7 @@ async function submitDiploma() {
   diplomaDiv.style.position = 'absolute';
   diplomaDiv.style.left = '-9999px';
   diplomaDiv.style.top = '0';
-  diplomaDiv.style.width = '800px';
+  diplomaDiv.style.width = '900px';
   diplomaDiv.style.height = '600px';
   diplomaDiv.style.backgroundImage = 'url(/diplom.jpg)';
   diplomaDiv.style.backgroundSize = 'cover';
